127c8e110ba491ade0f6ab1fb3d72d8b5ed9f7fb
Juliano Meinen <julianomeinen.souza@gmail.com>
2020-02-21 14:13:03 -0300

    Endpoints for new, current, next and set current

--- src/app/controller/SequenceController.js
+++ src/app/controller/SequenceController.js
@@ -1,13 +1,54 @@
 const Sequence = require("../model/Sequence");
 
 class SequenceController {
-
+  
+  // Save new sequence value
+  async store(req, res) {
+    const values = req.query ? req.query : req.body;
+    try{
+        const data = await Sequence.create(values);
+        return res.json(data);
+    } catch(err) {
+        return res.status(400).json(err);
+    }
+  }
+  
   // Get the current value
   async current(req, res) {
     const data = await Sequence.findOne({ _id: "5e4fd7885478d2293824f671" });
     return res.json(data);
   }
 
+  // Update the current value +1 and return new value
+  async next(req, res) {
+    
+    const current = await Sequence.findOne({ _id: "5e4fd7885478d2293824f671" });
+    current.integer+=1;
+
+    const data = await Sequence.updateOne({ _id: "5e4fd7885478d2293824f671" }, {
+        integer: current.integer
+    },
+    function(err) {
+       if(err)
+            return res.status(400).json(err);
+        return res.json(current);
+    })
+  }
+
+  // Set a new value to current
+  async setCurrent(req, res) {
+    const values = req.query ? req.query : req.body;
+    const data = await Sequence.updateOne({ _id: "5e4fd7885478d2293824f671" }, {
+        integer: values.integer
+    }, 
+    { runValidators: true }, 
+    function(err, affected, resp) {
+       if(err){
+            return res.status(400).json(err);
+       }
+        return res.json(affected);
+    })
+  }
 }
 
 module.exports = new SequenceController();
--- src/routes.js
+++ src/routes.js
@@ -8,5 +8,10 @@ routes.get("/", function(req, res) {
 const SequenceController = require("./app/controller/SequenceController");
 
 routes.get("/current", SequenceController.current);
+routes.post("/current", SequenceController.setCurrent);
+routes.get("/next", SequenceController.next);
+
+// Just test
+// routes.post("/new", SequenceController.store);
 
 module.exports = routes;
